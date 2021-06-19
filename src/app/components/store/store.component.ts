1e33aae8ba000886db47cbecfff2ba2229938410
Katya Lazorenko <katyalazorenko@MacBook-Pro-Katya.local>
2021-06-19 17:27:07 +0300

    Initial

---
+++ src/app/components/store/store.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "store",
+    templateUrl: "store.component.html",
+    styleUrls: ["store.component.styl"]
+})
+
+export class StoreComponent {
+    constructor() {}
+}
